8039b5b13944f2cd078b04fbd95282640cd92da4
Thunderll <liulei911222@163.com>
2017-12-24 16:47:03 +0800

    设置限速

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 from scrapy.cmdline import execute
 
 
-# execute(["scrapy", "crawl", "jobbole"])
-execute(["scrapy", "crawl", "zhihu"])
+execute(["scrapy", "crawl", "jobbole"])
+# execute(["scrapy", "crawl", "zhihu"])
 # execute(["scrapy", "crawl", "lagou"])
